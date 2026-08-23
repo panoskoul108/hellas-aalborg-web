e4e6a139dc93583b139339f66039e252e87ab912
panoskoul108 <235298513+panoskoul108@users.noreply.github.com>
2026-08-24 00:39:58 +0300

    Update supabase.js

--- app/supabase.js
+++ app/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://keolpijcsvwsrzkjqtkc.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtlb2xwaWpjc3Z3c3J6a2pxdGtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc0OTk0NzMsImV4cCI6MjEwMzA3NTQ3M30.yrxVzfswzX653cgYaZ5Kq1HBTmYyjjD64GB9oFhJXzc';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
